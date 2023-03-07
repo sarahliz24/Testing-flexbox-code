c78f4bb980fa8384d8b391ac831020b874de6880
sarahliz24 <sarahliz24@hotmail.com>
2023-03-07 17:23:18 +0000

    testing 3

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,50 @@
 //Global variables//
 const letsPlay = document.getElementById("lets-play");
-//const scoreTracker = document.getElementsByClassName("score-tracker");
+const scoreTracker = document.getElementsByClassName("score-tracker");
 const questionBox = document.getElementsByClassName("question-box");
-//const optionsContainer = document.getElementsByClassName("options-container");
-const questionOption = Array.from(document.querySelectorAll("question-option"));
+const optionsContainer = document.getElementsByClassName("options-container");
+let answerOption = Array.from(document.getElementsByClassName("answer-option"));
 const home = document.getElementsByClassName("home");
 const game = document.getElementsByClassName("game");
-//const endPage = document.getElementsByClassName("end-page");
+const endPage = document.getElementsByClassName("end-page");
 const homeDisplay = document.getElementById("home-display");
 const gameDisplay = document.getElementById("game-display");
 //const options = Array.from(document.querySelectorAll('option'))
 
-//let next;
+let next;
 let currentQuestion = {};
 let questionCounter = 0;
 let availableQuestions = [];
-let acceptingAnswers = true
+let acceptingAnswers;
 const maxQuestions = 3;
-//let score;
+let score =0;
+
+/* Questions array */
+let questionsArray = [{
+    question: "Movie acting suits me because I only need to be good for 90 seconds at a time",
+    option1: "Ryan Reynolds",
+    option2: "Angelina Jolie",
+    option3: "Bill Murray",
+    option4: "Arnold Schwarzenegger",
+    answer: "3",
+},
+{
+    question: "Being a father is the single greatest feeling on earth. Not including those wonderful years I spent without a child, of course",
+    option1: "Zach Galifianakis",
+    option2: "Ryan Reynolds",
+    option3: "Bill Murray",
+    option4: "Arnold Schwarzenegger",
+    answer: "2",
+},
+{
+    question: "Get at least eight hours of beauty sleep, nine if you’re ugly",
+    option1: "Christina Aguilera",
+    option2: "Tara Reid",
+    option3: "Marilyn Monroe",
+    option4: "Betty White",
+    answer: "4",
+},
+]
 
 /* Displays and hides sections as advancing through gameplay via button clicks*/
 letsPlay.addEventListener("click", () => {
@@ -33,8 +60,10 @@ document.addEventListener("DOMContentLoaded", function() {
 let startGame = () => {
     questionCounter = 0;
     //document.questionOption.innerHTML = questions[currentQuestion].question;
+    score = 0;
     availableQuestions = [...questionsArray];
-    getQuestion()
+    console.log(availableQuestions);
+    getQuestion();
 }
 
 let getQuestion = () => {
@@ -44,48 +73,20 @@ let getQuestion = () => {
         return window.location.assign('about.html')
     }
 
-   questionCounter = ++
+    //questionCounter = ++
 
-    const questionsIndex = Math.floor(Math.random() * availableQuestions.length);
-    currentQuestion = availableQuestions[questionsIndex];
-    questionBox.innerHTML = currentQuestion.question;
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    questionBox.innerHMTL = currentQuestion.question;
 
-    questionOption.forEach((option) => {
-        const number = option.dataset.option;
-        option.innerHTML = currentQuestion['option' + number];
+    answerOption.forEach((option) => {
+        const number = option.dataset["number"];
+        option.innerText = currentQuestion['option' + number];
     })
 
-    availableQuestions.splice(questionsIndex, 1);
+    availableQuestions.splice(questionIndex, 1);
 
     acceptingAnswers = true;
 }
 
 startGame();
-
-/* Questions array */
-
-let questionsArray = [{
-        question: "Movie acting suits me because I only need to be good for 90 seconds at a time",
-        option1: "Ryan Reynolds",
-        option2: "Angelina Jolie",
-        option3: "Bill Murray",
-        option4: "Arnold Schwarzenegger",
-        answer: "3",
-    },
-    {
-        question: "Being a father is the single greatest feeling on earth. Not including those wonderful years I spent without a child, of course",
-        option1: "Zach Galifianakis",
-        option2: "Ryan Reynolds",
-        option3: "Bill Murray",
-        option4: "Arnold Schwarzenegger",
-        answer: "2",
-    },
-    {
-        question: "Get at least eight hours of beauty sleep, nine if you’re ugly",
-        option1: "Christina Aguilera",
-        option2: "Tara Reid",
-        option3: "Marilyn Monroe",
-        option4: "Betty White",
-        answer: "4",
-    },
-]
