ffea386e6e4f9f6420a2e2c09f1e6f60a970c0aa
sarahliz24 <sarahliz24@hotmail.com>
2023-03-10 21:34:52 +0000

    Useless testing 10 march

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,14 +10,14 @@ const endPage = document.getElementsByClassName("end-page");
 const homeDisplay = document.getElementById("home-display");
 const gameDisplay = document.getElementById("game-display");
 let userAnswer = document.getElementsByTagName("span");
-let correctAnswer = document.getElementsByClassName("correct-answer");
-let wrongAnswer = document.getElementsByClassName("wrong-answer");
+const correctAnswer = document.getElementsByClassName("correct-answer");
+const wrongAnswer = document.getElementsByClassName("wrong-answer");
 //const theQuestion = document.getElementByI("theQuestion");
 //const options = Array.from(document.querySelectorAll('option'))
 
 let next;
 let currentQuestion = {};
-let questionCounter = 0;
+//let questionCounter = 0;
 let availableQuestions = [...questionsArray];
 //let acceptingAnswers = false;
 const maxQuestions = 3;
@@ -86,12 +86,13 @@ let getQuestion = () => {
 
 /* Turns answer box 3 background colour red*/
 //code from https://www.w3schools.com/jsref/met_element_getelementsbytagname.asp
-const spanDiv = document.getElementById("spanDiv");
-    spanDiv.getElementsByTagName("*")[2].style.backgroundColor = "red";
+//const spanDiv = document.getElementById("spanDiv");
+   // spanDiv.getElementsByTagName("*")[2].style.backgroundColor = "red";
 
-/* let checkAnswer = () => {
+//check answer
+/*let checkAnswer = () => {
 
-    for (i = 0; i < userAnswer.length; i++) {
+    for (i = 0; i < uanswerOption.length; i++) {
         userAnswer[i].onclick = showAnswer;
     }
 }
@@ -106,6 +107,60 @@ let showAnswer = () => {
     } else {
         answerOption.classList.add("wrong-answer");
     }
-}*/
+}; */
+
+//check answer
+/*answerOption.forEach((option) => {
+
+    answerOption.addEventListener("click", e => {
+
+        const correctAnswer = option.dataset["answer"];
+
+        if (correctAnswer == answer) {
+            correctAnswer.classList.add("correct-answer");
+        } else {
+            classList.add("wrong-answer");
+        }
+    }
+)}); */
+        
+
+//let wrongAnswer = document.querySelector('.wrong-answer')
+/*correctAnswer = answerOption.dataset["answer"];
+const spanDiv = document.getElementById("spanDiv");
+
+body.addEventListener('click', span => {
+  if (span.target !== wrongAnswer && span.target !== correctAnswer) {
+    return
+  } else if (span.target == wrongAnswer) {
+        span.classList.add("wrong-answer");
+  } else {
+    span.classList.add("correct-answer");
+  }
+});
+
+const spanDiv = document.getElementById("spanDiv");
+    spanDiv.getElementsByTagName("*")[2].style.backgroundColor = "red";*/
+
+ /*   answerOption.addEventListener('click', e => {
+        if(e == currentQuestion.answer) {
+            let correctAnswer = document.getElementsByClassName("correct-answer");
+            correctAnswer.classList.add("correct-answer");
+        }
+}); */
+
 
 startGame();
+
+let questionCounter = 0;
+const questionIndex = Math.floor(Math.random() * availableQuestions.length); 
+currentQuestion = availableQuestions[questionIndex];
+let display = document.querySelector('spanDiv')
+display.innerText = currentQuestion[questionCounter];
+
+let nextButton = document.querySelector('next-button');
+
+nextButton.addEventListener('click', () => {
+    questionCounter++;
+    display.innerText = currentQuestion[questionCounter];
+})
